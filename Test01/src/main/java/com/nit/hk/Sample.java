14b92e3941edeb96604bd6bb43ea4267182dded4
HP <HP@chanti>
2025-01-10 11:40:41 +0530

    inital commit

---
+++ Test01/src/main/java/com/nit/hk/Sample.java
@@ -0,0 +1,8 @@
+package com.nit.hk;
+
+public class Sample {public Sample() {
+	// TODO Auto-generated constructor stub
+}public static void main(String[] args) {
+	System.out.println("hiii from sample class");
+}
+}
